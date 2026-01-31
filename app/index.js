1d9a2c9b7b1dc9fab86f894677da37958e9b3a4c
amaan144 <amaanahamed24@gmail.com>
2026-01-31 07:28:32 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -3,13 +3,13 @@ const app = express();
 const PORT = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello! This is Tanya''s Cloud Assignment.'); 
+  res.send("Hello! This is Tanya's Cloud Assignment.");
 });
 
 app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
 
-app.listen(PORT, () => {
-  console.log('Server running on port ' + PORT);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
